b6888e5045fbdf6b168b25638a1bfa30931edb88
montmurray <vinylscratch1103@gmail.com>
2025-04-14 09:31:42 -0500

    Initial Commit

---
+++ CollaborativeProject/CollaborativeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CollaborativeProjectApp.swift
+//  CollaborativeProject
+//
+//  Created by Tessa Murray on 4/14/25.
+//
+
+import SwiftUI
+
+@main
+struct CollaborativeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
