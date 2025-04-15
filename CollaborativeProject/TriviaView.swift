a4465c06620e5a4ffcf3b2e2ee3d2b31e5c00f63
dmyers1048 <dmyers1@bsd220.org>
2025-04-15 10:09:41 -0500

    Added Trivia Questions View

---
+++ CollaborativeProject/TriviaView.swift
@@ -0,0 +1,18 @@
+//
+//  TriviaView.swift
+//  CollaborativeProject
+//
+//  Created by Devan Myers on 4/15/25.
+//
+
+import SwiftUI
+
+struct TriviaView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TriviaView()
+}
